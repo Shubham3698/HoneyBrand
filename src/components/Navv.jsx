13eed34e5edb05ccb3abacd3d1ad5affc5b92302
Shubham Pandey <Pandey0shubham3698@gmail.com>
2024-11-05 21:30:21 +0530

    navv.jsx me side bar sahi kiya

--- src/components/Navv.jsx
+++ src/components/Navv.jsx
@@ -1,6 +1,6 @@
 // Navv.js
 import React, { useContext } from 'react';
-import { Navbar, Nav, Container, Form, FormControl, Button, Badge } from 'react-bootstrap';
+import { Navbar, Nav, Container, Form, FormControl, Button, Badge, Offcanvas } from 'react-bootstrap';
 import { FaUser, FaShoppingCart } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import logo from './../assets/Logo.webp';
@@ -14,9 +14,8 @@ function Navv() {
       bg="dark"
       variant="dark"
       expand="lg"
+      fixed="top"
       style={{
-        position: 'fixed',
-        top: 0,
         width: '100%',
         zIndex: 1000,
       }}
@@ -31,43 +30,54 @@ function Navv() {
             className="d-inline-block align-top"
           />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="/Our-Products">Our products</Nav.Link>
-            <Nav.Link as={Link} to="/Our-Story">Our story</Nav.Link>
-            <Nav.Link as={Link} to="/Contact">Contact</Nav.Link>
-            <Nav.Link as={Link} to="/Track-order">Track order</Nav.Link>
-          </Nav>
+        <Navbar.Toggle aria-controls="offcanvasNavbar" />
+        
+        {/* Offcanvas for Sidebar */}
+        <Navbar.Offcanvas
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+          placement="start"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id="offcanvasNavbarLabel">Menu</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/Our-Products">Our products</Nav.Link>
+              <Nav.Link as={Link} to="/Our-Story">Our story</Nav.Link>
+              <Nav.Link as={Link} to="/Contact">Contact</Nav.Link>
+              <Nav.Link as={Link} to="/Track-order">Track order</Nav.Link>
+            </Nav>
 
-          {/* Search bar with a button */}
-          <Form className="d-flex me-3">
-            <FormControl
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
+            {/* Search bar with a button */}
+            <Form className="d-flex me-3">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
 
-          {/* Account and Cart Icons */}
-          <Nav>
-            <Nav.Link as={Link} to="/Account" className="d-flex align-items-center">
-              <FaUser style={{ marginRight: '5px' }} /> Account
-            </Nav.Link>
-            <Nav.Link as={Link} to="/Cart" className="d-flex align-items-center">
-              <FaShoppingCart style={{ marginRight: '5px' }} />
-              Cart
-              {cartCount > 0 && (
-                <Badge bg="danger" pill style={{ marginLeft: '5px' }}>
-                  {cartCount}
-                </Badge>
-              )}
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+            {/* Account and Cart Icons */}
+            <Nav>
+              <Nav.Link as={Link} to="/Account" className="d-flex align-items-center">
+                <FaUser style={{ marginRight: '5px' }} /> Account
+              </Nav.Link>
+              <Nav.Link as={Link} to="/Cart" className="d-flex align-items-center">
+                <FaShoppingCart style={{ marginRight: '5px' }} />
+                Cart
+                {cartCount > 0 && (
+                  <Badge bg="danger" pill style={{ marginLeft: '5px' }}>
+                    {cartCount}
+                  </Badge>
+                )}
+              </Nav.Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
       </Container>
     </Navbar>
   );
